b2b7e68b4fda3ec712c0e2b86c678ebed988f948
cids <sidc646@gmail.com>
2026-01-23 00:15:23 -0800

    example issue - sc (sorry i got delayed)

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,16 +1,20 @@
 const Example = (props: { text1: string; text2: string; text3: string }) => {
   return (
-    <div className="bg-ridesucr-black flex w-3/4 items-center gap-1 justify-self-center p-8">
-      <div className="bg-ridesucr-white w-1/2 p-5 text-left">{props.text1}</div>
-      <div className="flex w-1/2 flex-row gap-1">
-        <div className="bg-ridesucr-white w-full p-5 text-left">
-          props.text2
+    <div className="bg-ridesucr-black flex w-2/3 flex-col items-center gap-4 justify-self-center p-8 text-center text-3xl">
+      <div className="bg-ridesucr-white w-1/2 rounded p-5 md:rounded-lg">
+        {props.text1}
+      </div>
+      <div className="flex w-1/2 flex-row gap-5">
+        <div className="bg-ridesucr-white w-full rounded-full p-5">
+          {props.text2}
         </div>
-        <div className="bg-ridesucr-white w-full p-5 text-right">
+        <div className="bg-ridesucr-white w-full rounded-full p-5 text-white">
           {props.text2}
         </div>
       </div>
-      {/* Hint: Add another box/div here similarly to line 4 */}
+      <div className="bg-ridesucr-white w-1/2 rounded p-5 md:rounded-lg">
+        {props.text1}
+      </div>
     </div>
   );
 };
