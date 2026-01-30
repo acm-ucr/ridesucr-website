c86c56e1010c71e78aebb6c24682f5c172e7c31e
Kel Umozurike <Kelechiumozurike@gmail.com>
2026-01-30 13:59:08 -0800

    Format files with Prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,31 +11,28 @@ import Example from "@/components/Example";
 //export default Home;
 const Home = () => {
   return (
- <div className="flex h-screen w-screen items-center justify-center">
-      
-     <div className="flex w-2/3 flex-col gap-6 rounded-lg p-6">
-        {/* Inner boxes */} 
-        
-        <div className="flex items-center justify-center rounded-md p-6 text-2xl box1">
-           style={{ backgroundColor: "var(--color-ridesucr-lightred)" }}
-           
-        box 1
-        </div>
-       
-        <div className="flex items-center justify-center rounded-full p-6 text-2xl box2">
-           style={{ backgroundColor: "var(--color-ridesucr-orange)" }}
-           
-           box 2
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="flex w-2/3 flex-col gap-6 rounded-lg p-6">
+        {/* Inner boxes */}
+
+        <div className="box1 flex items-center justify-center rounded-md p-6 text-2xl">
+          style={{ backgroundColor: "var(--color-ridesucr-lightred)" }}
+          box 1
         </div>
 
-       <div className="flex items-center justify-center rounded-md p-6 text-2xl box3">
-           style={{ backgroundColor: "var(--color-ridesucr-mintgreen)" }}
-           box 3
+        <div className="box2 flex items-center justify-center rounded-full p-6 text-2xl">
+          style={{ backgroundColor: "var(--color-ridesucr-orange)" }}
+          box 2
         </div>
 
-        <div className="flex items-center justify-center rounded-full p-6 text-2xl text-white box4">
-           style={{ backgroundColor: "var(--color-ridesucr-blue)" }}
-           box 4
+        <div className="box3 flex items-center justify-center rounded-md p-6 text-2xl">
+          style={{ backgroundColor: "var(--color-ridesucr-mintgreen)" }}
+          box 3
+        </div>
+
+        <div className="box4 flex items-center justify-center rounded-full p-6 text-2xl text-white">
+          style={{ backgroundColor: "var(--color-ridesucr-blue)" }}
+          box 4
         </div>
       </div>
     </div>
