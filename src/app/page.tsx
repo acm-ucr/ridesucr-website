bc0342b1980a63a3eb961f7806bce50eebd8de8d
Kel Umozurike <Kelechiumozurike@gmail.com>
2026-01-30 13:10:34 -0800

    I updated the colors on global css and I updated the page layout on page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,43 @@
 import Example from "@/components/Example";
 
-const Home = () => {
+/*const Home = () => {
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center">
       <Example text1="Adjust" text2="These" text3="Props" />
+    </div>
+  );
+};*/
+
+//export default Home;
+const Home = () => {
+  return (
+ <div className="flex h-screen w-screen items-center justify-center">
+      
+     <div className="flex w-2/3 flex-col gap-6 rounded-lg p-6">
+        {/* Inner boxes */} 
+        
+        <div className="flex items-center justify-center rounded-md p-6 text-2xl box1">
+           style={{ backgroundColor: "var(--color-ridesucr-lightred)" }}
+           
+        box 1
+        </div>
+       
+        <div className="flex items-center justify-center rounded-full p-6 text-2xl box2">
+           style={{ backgroundColor: "var(--color-ridesucr-orange)" }}
+           
+           box 2
+        </div>
+
+       <div className="flex items-center justify-center rounded-md p-6 text-2xl box3">
+           style={{ backgroundColor: "var(--color-ridesucr-mintgreen)" }}
+           box 3
+        </div>
+
+        <div className="flex items-center justify-center rounded-full p-6 text-2xl text-white box4">
+           style={{ backgroundColor: "var(--color-ridesucr-blue)" }}
+           box 4
+        </div>
+      </div>
     </div>
   );
 };
