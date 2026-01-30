9436789ecb5ab885d0e4bfaf920a8168d79390ea
ALE00111 <austintle56@gmail.com>
2026-01-30 12:20:50 -0800

    example

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -2,14 +2,7 @@ const Example = (props: { text1: string; text2: string; text3: string }) => {
   return (
     <div className="bg-ridesucr-black flex w-3/4 items-center gap-1 justify-self-center p-8">
       <div className="bg-ridesucr-white w-1/2 p-5 text-left">{props.text1}</div>
-      <div className="flex w-1/2 flex-row gap-1">
-        <div className="bg-ridesucr-white w-full p-5 text-left">
-          props.text2
-        </div>
-        <div className="bg-ridesucr-white w-full p-5 text-right">
-          {props.text2}
-        </div>
-      </div>
+
       {/* Hint: Add another box/div here similarly to line 4 */}
     </div>
   );
