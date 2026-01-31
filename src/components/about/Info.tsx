50345cb19dd1de11b23e2f11881ae53a853c74e7
ALE00111 <austintle56@gmail.com>
2026-01-31 12:36:36 -0800

    Tweaks

--- src/components/about/Info.tsx
+++ src/components/about/Info.tsx
@@ -1,7 +1,9 @@
 const Info = () => {
   return (
-    <div className="flex w-1/2 gap-4 justify-self-center rounded-md bg-white/30 p-8 text-center text-2xl text-ridesucr-white backdrop-blur-lg">
-      RIDES @ UCR IS A STUDENT-LED CLUB THAT AIMS TO BRING THE CAR COMMUNITY TOGETHER. WE ORGANIZE WEEKLY EVENTS, SUCH AS CAR MEETS, WITH THE PURPOSE OF FOSTERING A COMMUNITY OF ENTHUSIASTS
+    <div className="text-ridesucr-white flex w-1/2 gap-4 justify-self-center rounded-md bg-white/30 p-8 text-center text-2xl backdrop-blur-lg">
+      RIDES @ UCR IS A STUDENT-LED CLUB THAT AIMS TO BRING THE CAR COMMUNITY
+      TOGETHER. WE ORGANIZE WEEKLY EVENTS, SUCH AS CAR MEETS, WITH THE PURPOSE
+      OF FOSTERING A COMMUNITY OF ENTHUSIASTS
     </div>
   );
 };
