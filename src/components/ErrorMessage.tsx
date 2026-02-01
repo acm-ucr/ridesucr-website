713ee5dc41992cef615632188638c71faa74ee3f
Celina Vasquez <cvasq043@ucr.edu>
2026-02-01 14:53:12 -0800

    Edited 404 Page for comments

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -8,20 +8,20 @@ interface ErrorMessageProps {
 const ErrorMessage = ({ text }: ErrorMessageProps) => {
   return (
     <div className="bg-ridesucr-black flex h-screen w-screen items-center">
-      <div className="flex w-full flex-col items-center px-40">
-        <div className="flex w-full items-center justify-between">
-          <div className="relative z-10 pt-16 pb-10">
+      <div className="flex w-full flex-col items-center px-60">
+        <div className="flex w-full">
+          <div className="relative z-10 pt-16 pb-10 right-20">
             <div className="text-ridesucr-white text-[20rem] leading-none">
               {text}
             </div>
             <p className="text-ridesucr-gray pb-2 text-5xl">OFF THE ROAD.</p>
             <p className="text-ridesucr-white text-5xl">RETURN HOME?</p>
           </div>
-          <div className="mt-20 pt-16">
+          <div className="mt-20 ml-10">
             <Image
               src={ErrorImage}
               alt="Check Engine Light: Error Message Picture"
-              className="h-96 max-w-80 blur-[0.3px] [mask-image:radial-gradient(circle_at_center,black_5%,transparent_65%)]"
+              className="h-[400px] w-[600px] blur-[0.3px] [mask-image:radial-gradient(circle_at_center,black_5%,transparent_65%)]"
             ></Image>
           </div>
         </div>
