c4cfb7517df53c57c5377767d713e1f254f9f240
ALE00111 <austintle56@gmail.com>
2026-02-02 11:11:13 -0800

    Fix/changes

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -7,26 +7,22 @@ interface ErrorMessageProps {
 
 const ErrorMessage = ({ text }: ErrorMessageProps) => {
   return (
-    <div className="bg-ridesucr-black flex h-screen w-screen items-center">
-      <div className="flex w-full flex-col items-center px-60">
-        <div className="flex w-full">
-          <div className="relative right-20 z-10 pt-16 pb-10">
-            <div className="text-ridesucr-white text-[20rem] leading-none">
-              {text}
-            </div>
-            <p className="text-ridesucr-gray pb-2 text-5xl">OFF THE ROAD.</p>
-            <p className="text-ridesucr-white text-5xl">RETURN HOME?</p>
-          </div>
-          <div className="mt-20 ml-10">
-            <Image
-              src={ErrorImage}
-              alt="Check Engine Light: Error Message Picture"
-              className="h-[400px] w-[600px] blur-[0.3px] [mask-image:radial-gradient(circle_at_center,black_5%,transparent_65%)]"
-            ></Image>
-          </div>
+    <div className="bg-ridesucr-black flex h-screen flex-col items-center">
+      <div className="flex overflow-hidden px-[20vw]">
+        <div className="relative right-20 z-10 flex flex-col justify-center py-20">
+          <p className="text-ridesucr-white text-[25rem] leading-none">
+            {text}
+          </p>
+          <p className="text-ridesucr-gray pb-2 text-6xl">OFF THE ROAD.</p>
+          <p className="text-ridesucr-white text-6xl">RETURN HOME?</p>
         </div>
-        <div className="via-ridesucr-white mt-2 h-px w-2/4 bg-gradient-to-r from-transparent to-transparent"></div>
+        <Image
+          src={ErrorImage}
+          alt="Check Engine Light: Error Message Picture"
+          className="blur-[0.3px] [mask-image:radial-gradient(circle_at_center,black_5%,transparent_65%)]"
+        />
       </div>
+      <div className="via-ridesucr-white my-4 h-1 w-1/3 bg-gradient-to-r from-transparent to-transparent" />
     </div>
   );
 };
