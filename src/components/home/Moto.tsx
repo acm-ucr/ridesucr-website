1c4871e957dd059edaa9d57580a8a4c3025c4fbc
etan29 <emilytan2992@gmail.com>
2026-02-01 16:28:28 -0800

    create motto

--- src/components/home/Moto.tsx
+++ src/components/home/Moto.tsx
@@ -1,5 +1,15 @@
 const Moto = () => {
-  return <div>Moto</div>;
+  return (
+    <div className="text-left">
+      <h1 className="text-ridesucr-white text-6xl">
+        driven by
+        <br />
+        passion
+      </h1>
+
+      <p className="text-ridesucr-gray text-lg">we are rides @ ucr.</p>
+    </div>
+  );
 };
 
 export default Moto;
