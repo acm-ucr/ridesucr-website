366ed1dc9f22c7ce6a3cf49eab880764d3c552ab
ALE00111 <austintle56@gmail.com>
2026-02-03 19:06:19 -0800

    Tweaks

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,19 @@
 import Image from "next/image";
-import { RIDES_LINKS } from "../data/NavBar";
+import { RIDES_LINKS } from "../data/LinksData";
+import Link from "next/link"
 
 const Footer = () => {
   return (
-    <footer className="from-ridesucr-green to-ridesucr-black relative flex h-24 w-full items-end justify-between bg-linear-to-t px-10 pb-10">
+    <div className="from-ridesucr-green to-ridesucr-black relative flex h-24 w-full items-end justify-between bg-linear-to-t px-10 pb-10">
       <div className="from-ridesucr-white to-ridesucr-black absolute top-0 left-1/2 h-[1px] w-1/2 -translate-x-1/2 bg-radial" />
-      <div className="translate-y-5 text-3xl text-white md:text-5xl">
+      <div className="translate-y-5 text-white text-5xl">
         DRIVEN BY PASSION
       </div>
       <div className="flex translate-y-5 items-center gap-7">
         <div className="flex items-center gap-10">
           <div className="from-ridesucr-white/80 to-ridesucr-white/40 rounded-2xl bg-linear-to-b p-[1px]">
-            <div className="bg-ridesucr-black/70 flex items-center gap-5 rounded-2xl px-2 py-2 backdrop-blur-xs">
-              <a href={RIDES_LINKS.instagram} target="_blank" rel="noreferrer">
+            <div className="bg-ridesucr-black/70 flex items-center gap-5 rounded-2xl p-2 backdrop-blur-xs">
+              <Link href={RIDES_LINKS.instagram} target="_blank">
                 <Image
                   src="/Linkbox/Instagram.svg"
                   alt="Instagram"
@@ -20,12 +21,11 @@ const Footer = () => {
                   height={32}
                   className="h-8 w-8"
                 />
-              </a>
+              </Link>
 
-              <a
+              <Link
                 href={RIDES_LINKS.highlanderlink}
                 target="_blank"
-                rel="noreferrer"
               >
                 <Image
                   src="/Linkbox/HighlanderLink.svg"
@@ -34,9 +34,9 @@ const Footer = () => {
                   height={32}
                   className="h-8 w-8"
                 />
-              </a>
+              </Link>
 
-              <a href={RIDES_LINKS.discord} target="_blank" rel="noreferrer">
+              <Link href={RIDES_LINKS.discord} target="_blank">
                 <Image
                   src="/Linkbox/Discord.svg"
                   alt="Discord"
@@ -44,23 +44,23 @@ const Footer = () => {
                   height={32}
                   className="h-8 w-8"
                 />
-              </a>
+              </Link>
             </div>
           </div>
         </div>
 
-        <div className="flex flex-col items-center">
+        <Link href = "/" className="flex flex-col items-center">
           <Image
             src="/Logo.svg"
             alt="Logo"
             width={64}
             height={64}
-            className="h-auto w-16 object-contain invert"
-            priority // Prioritize the logo for brand visibility
+            className="h-auto w-16 invert"
+            priority 
           />
-        </div>
+        </Link>
       </div>
-    </footer>
+    </div>
   );
 };
 
