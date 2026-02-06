5ba91017c1c348ff7260f815c27162997d169170
Kel Umozurike <Kelechiumozurike@gmail.com>
2026-02-06 12:53:55 -0800

    Fixed the issues in title component and updated the three pages

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,5 +1,13 @@
-const Title = () => {
-  return <div>Title</div>;
+type TitleProps = {
+  text: string;
 };
+
+const Title = ({ text }: TitleProps) => {
+  return (
+    <div className="text-7xl flex items-center justify-center bg-black text-center text-white uppercase py-10">
+      {text}
+    </div>
+  );
+}
 
 export default Title;
