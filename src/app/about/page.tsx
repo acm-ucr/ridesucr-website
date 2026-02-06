6526953948166c4d79cbda53949e729d54a838d5
Kel Umozurike <Kelechiumozurike@gmail.com>
2026-02-06 12:53:55 -0800

    Fixed the issues in title component and updated the three pages

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,10 @@
 import InfoSection from "@/components/about/InfoSection";
+import Title from "@/components/Title";
 
 const About = () => {
   return (
     <div>
+      <Title text="About Us" />
       <InfoSection />
     </div>
   );
