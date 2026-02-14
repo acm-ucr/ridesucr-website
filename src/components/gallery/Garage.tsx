02b178f580211e2e318f9aa49f9db28e546b8ef2
Celina Vasquez <cvasq043@ucr.edu>
2026-02-13 16:46:10 -0800

    Created mobile responsiveness for gallery

--- src/components/gallery/Garage.tsx
+++ src/components/gallery/Garage.tsx
@@ -2,7 +2,7 @@ import CarsGrid from "@/components/gallery/CarsGrid";
 
 const Garage = () => {
   return (
-    <div className="ring-ridesucr-white/70 bg-ridesucr-gray/45 shadow-2x1 overflow-hidden rounded-xl p-[5px] ring-1 backdrop-blur-md">
+    <div className= "lg:ring-ridesucr-white/70 lg:bg-ridesucr-gray/45 lg:shadow-2x1 lg:overflow-hidden lg:rounded-xl lg:p-[5px] lg:ring-1 lg:backdrop-blur-md md:ring-0 md:bg-transparent md:shadow-none md:p-0 md:backdrop-blur-none">
       <CarsGrid />
     </div>
   );
