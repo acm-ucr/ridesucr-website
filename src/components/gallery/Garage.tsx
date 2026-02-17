0ac018cae9ba66b1d48e004f75a56e552aee3aa1
Celina Vasquez <cvasq043@ucr.edu>
2026-02-16 21:10:23 -0800

    Fixed comments

--- src/components/gallery/Garage.tsx
+++ src/components/gallery/Garage.tsx
@@ -2,7 +2,7 @@ import CarsGrid from "@/components/gallery/CarsGrid";
 
 const Garage = () => {
   return (
-    <div className="lg:ring-ridesucr-white/70 lg:bg-ridesucr-gray/45 lg:shadow-2x1 md:bg-transparent md:p-0 md:shadow-none md:ring-0 md:backdrop-blur-none lg:overflow-hidden lg:rounded-xl lg:p-[5px] lg:ring-1 lg:backdrop-blur-md">
+    <div className="lg:ring-ridesucr-white/70 lg:bg-ridesucr-gray/45 lg:shadow-2x1 lg:overflow-hidden lg:rounded-xl lg:p-[5px] lg:ring-1 lg:backdrop-blur-md">
       <CarsGrid />
     </div>
   );
