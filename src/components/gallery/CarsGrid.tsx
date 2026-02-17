9120771082a368bbf2e6e65aa7505be8cc1ea824
JeffreyXia05 <jeffx2016@gmail.com>
2026-02-17 14:00:13 -0800

    minor fixes

--- src/components/gallery/CarsGrid.tsx
+++ src/components/gallery/CarsGrid.tsx
@@ -7,10 +7,10 @@ const CarsGrid = () => {
       {galleryData.map((item, index) => (
         <div
           key={index}
-          className={`lg:border-ridesucr-white ring-ridesucr-white/70 bg-ridesucr-gray/45 flex items-center justify-center overflow-hidden rounded-xl border p-[5px] shadow-2xl ring-1 backdrop-blur-md lg:rounded-none lg:bg-transparent lg:p-0 lg:shadow-none lg:ring-0 ${index == 0 ? "lg:rounded-tl-md" : ""} ${index == 1 ? "" : ""} ${index == 2 ? "lg:rounded-tr-md" : ""} ${index == 3 ? "" : ""} ${index == 4 ? "" : ""} ${index == 5 ? "" : ""} ${index == 6 ? "lg:rounded-bl-md" : ""} ${index == 7 ? "" : ""} ${index == 8 ? "lg:rounded-br-md" : ""} `}
+          className={`lg:border-ridesucr-white ring-ridesucr-white/70 bg-ridesucr-gray/45 flex items-center justify-center overflow-hidden rounded-xl border p-[5px] shadow-2xl ring-1 backdrop-blur-md lg:rounded-none lg:bg-transparent lg:p-0 lg:shadow-none lg:ring-0 ${index == 0 ? "lg:rounded-tl-md" : ""} ${index == 2 ? "lg:rounded-tr-md" : ""} ${index == 6 ? "lg:rounded-bl-md" : ""} ${index == 8 ? "lg:rounded-br-md" : ""} `}
         >
           <Image
-            className={`${"flex h-full w-full items-center justify-center rounded-xl object-cover lg:rounded-none"}`}
+            className={`${"flex h-full w-full rounded-xl object-cover lg:rounded-none"}`}
             src={item.Pictures}
             alt={item.Alt}
           />
