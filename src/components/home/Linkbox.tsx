1c48280efda7b90264cd7fc121fa62d47901696f
etan29 <emilytan2992@gmail.com>
2026-02-19 20:04:15 -0800

    format

--- src/components/home/Linkbox.tsx
+++ src/components/home/Linkbox.tsx
@@ -5,11 +5,7 @@ const Linkbox = () => {
   return (
     <div className="text-ridesucr-white flex h-[30vh] w-[25vw] flex-col justify-evenly rounded-md bg-white/30 p-8 text-center text-2xl underline underline-offset-1 backdrop-blur-lg">
       {Object.entries(Rides_Links).map(([key, { name, url }]) => (
-        <Link
-          key={key}
-          href={url}
-          target="_blank"
-        >
+        <Link key={key} href={url} target="_blank">
           {name}
         </Link>
       ))}
