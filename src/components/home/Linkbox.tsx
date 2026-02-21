b8f21e1c350a5c945fd8eb73e4dfa4a02b52ca8f
ALE00111 <austintle56@gmail.com>
2026-02-21 12:15:17 -0800

    Changes

--- src/components/home/Linkbox.tsx
+++ src/components/home/Linkbox.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Linkbox = () => {
   return (
     <div className="text-ridesucr-white flex h-[30vh] w-[25vw] flex-col justify-evenly rounded-md bg-white/30 p-8 text-center text-2xl underline underline-offset-1 backdrop-blur-lg">
-      {Rides_Links.map(({name, url}) => (
+      {Rides_Links.map(({ name, url }) => (
         <Link key={name} href={url} target="_blank">
           {name}
         </Link>
