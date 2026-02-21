ff463410807468dea3d6df1bfecf64dab9f6d0a3
Celina Vasquez <cvasq043@ucr.edu>
2026-02-20 16:16:21 -0800

    Created mobile responsiveness for 404 page

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -9,13 +9,17 @@ interface ErrorMessageProps {
 const ErrorMessage = ({ text }: ErrorMessageProps) => {
   return (
     <div className="bg-ridesucr-black flex h-screen flex-col items-center">
-      <div className="flex overflow-hidden px-[20vw]">
-        <div className="z-10 flex flex-col justify-center">
-          <p className="text-ridesucr-white text-[25rem]/none">{text}</p>
-          <p className="text-ridesucr-gray pb-2 text-6xl">OFF THE ROAD.</p>
+      <div className="relative flex h-lvh flex-col overflow-hidden px-[10vw] text-center lg:flex-row lg:px-[20vw]">
+        <div className="relative z-10 flex flex-col justify-center">
+          <p className="text-ridesucr-white text-center text-[15rem] lg:text-[25rem]/none">
+            {text}
+          </p>
+          <p className="text-ridesucr-gray relative z-20 pt-12 pb-2 text-center text-4xl lg:pt-0 lg:text-left lg:text-6xl">
+            OFF THE ROAD.
+          </p>
           <Link
             href="/"
-            className="text-ridesucr-white text-6xl underline hover:text-gray-400"
+            className="text-ridesucr-white relative z-20 text-center text-4xl underline hover:text-gray-400 lg:pt-0 lg:text-left lg:text-6xl"
           >
             RETURN HOME?
           </Link>
@@ -23,10 +27,9 @@ const ErrorMessage = ({ text }: ErrorMessageProps) => {
         <Image
           src={ErrorImage}
           alt="Check Engine Light: Error Message Picture"
-          className="blur-[0.3px] [mask-image:radial-gradient(circle_at_center,black_5%,transparent_65%)]"
+          className="absolute top-36 right-3 left-3 h-[450px] w-[600px] blur-[0.3px] [mask-image:radial-gradient(circle_at_center,black_5%,transparent_65%)] lg:static lg:top-auto lg:h-auto lg:w-auto"
         />
       </div>
-      <div className="via-ridesucr-white my-4 h-1 w-1/3 bg-gradient-to-r from-transparent to-transparent" />
     </div>
   );
 };
