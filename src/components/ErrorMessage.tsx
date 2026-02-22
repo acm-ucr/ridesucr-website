14261616f38fc629dfbc7d67459973f9c0cc5db0
JeffreyXia05 <jeffx2016@gmail.com>
2026-02-21 22:02:08 -0800

    small fix

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -8,10 +8,10 @@ interface ErrorMessageProps {
 
 const ErrorMessage = ({ text }: ErrorMessageProps) => {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center">
-      <div className="text-center lg:px-[15vw]">
-        <div className="lg:flex lg:justify-center">
-          <p className="text-ridesucr-white text-[15rem] lg:text-[25rem]/none">
+    <div className="bg-ridesucr-black flex h-screen items-start">
+      <div className="relative flex flex-col overflow-hidden px-[10vw] text-center lg:flex-row lg:px-[20vw]">
+        <div className="relative z-10 flex flex-col justify-center">
+          <p className="text-ridesucr-white text-center text-[15rem] lg:text-[25rem]/none">
             {text}
           </p>
 
@@ -31,8 +31,13 @@ const ErrorMessage = ({ text }: ErrorMessageProps) => {
           >
             RETURN HOME?
           </Link>
-        </div>     
-      </div>  
+        </div>
+        <Image
+          src={ErrorImage}
+          alt="Check Engine Light: Error Message Picture"
+          className="absolute top-38 right-3 left-3 h-[53vh] w-full blur-[0.3px] [mask-image:radial-gradient(circle_at_center,black_5%,transparent_65%)] lg:static lg:top-auto lg:h-auto lg:w-auto"
+        />
+      </div>
     </div>
   );
 };
