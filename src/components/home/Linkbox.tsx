55cbd4d698ad34a42a185205350c9da69b4db067
etan29 <emilytan2992@gmail.com>
2026-02-22 22:03:22 -0800

    format

--- src/components/home/Linkbox.tsx
+++ src/components/home/Linkbox.tsx
@@ -7,18 +7,18 @@ import Logo from "@/public/Logo-White.svg";
 const Linkbox = () => {
   return (
     <div className="flex flex-col items-center">
-      <div className="block md:hidden mb-4">
-        <Image src={Logo} alt="Logo" className="mb-4"/>
+      <div className="mb-4 block md:hidden">
+        <Image src={Logo} alt="Logo" className="mb-4" />
         <Image src={Est_2021} alt="Est 2021" className="ml-7" />
       </div>
-    <div className="text-ridesucr-white flex h-[40vh] w-[80vw] sm:w-[60vw] md:w-[40vw] lg:w-[25vw] flex-col justify-evenly rounded-lg border-[0.5px] border-white bg-white/15 text-center text-5xl underline underline-offset-1 backdrop-blur-lg">
-      {Rides_Links.map(({ name, url }) => (
-        <Link key={name} href={url} target="_blank">
-          {name}
-        </Link>
-      ))}
+      <div className="text-ridesucr-white flex h-[40vh] w-[80vw] flex-col justify-evenly rounded-lg border-[0.5px] border-white bg-white/15 text-center text-5xl underline underline-offset-1 backdrop-blur-lg sm:w-[60vw] md:w-[40vw] lg:w-[25vw]">
+        {Rides_Links.map(({ name, url }) => (
+          <Link key={name} href={url} target="_blank">
+            {name}
+          </Link>
+        ))}
+      </div>
     </div>
-  </div>
   );
 };
 
