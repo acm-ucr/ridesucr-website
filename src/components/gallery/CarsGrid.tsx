501b6009e0040ac281dc7ee6e44e2f87f7d4e12f
Celina Vasquez <cvasq043@ucr.edu>
2026-02-26 14:53:48 -0800

    Made car images in grid bigger while hovering

--- src/components/gallery/CarsGrid.tsx
+++ src/components/gallery/CarsGrid.tsx
@@ -1,13 +1,21 @@
 "use client";
+<<<<<<< HEAD
 
+=======
+>>>>>>> d8e1805 (Made car images in grid bigger while hovering)
 import { motion } from "motion/react";
 import Image from "next/image";
 import { galleryData } from "@/data/galleryData";
+
+const imageAnimation = () => ({
+  whileHover: { scale: 1.05 },
+});
 
 const CarsGrid = () => {
   return (
     <div className="grid h-auto grid-cols-1 gap-5 lg:h-[76rem] lg:grid-cols-3 lg:grid-rows-3 lg:gap-x-1.5 lg:gap-y-1.5">
       {galleryData.map((item, index) => (
+<<<<<<< HEAD
         <motion.div
           key={index}
           whileHover={{ scale: 1.1, zIndex: 70 }}
@@ -19,6 +27,19 @@ const CarsGrid = () => {
             src={item.Pictures}
             alt={item.Alt}
           />
+=======
+        <motion.div key={index} {...imageAnimation()} className="">
+          <div
+            key={index}
+            className={`lg:border-ridesucr-white ring-ridesucr-white/70 bg-ridesucr-gray/45 flex items-center justify-center overflow-hidden rounded-xl border p-[5px] shadow-2xl ring-1 backdrop-blur-md lg:rounded-none lg:bg-transparent lg:p-0 lg:shadow-none lg:ring-0 ${index == 0 ? "lg:rounded-tl-md" : ""} ${index == 2 ? "lg:rounded-tr-md" : ""} ${index == 6 ? "lg:rounded-bl-md" : ""} ${index == 8 ? "lg:rounded-br-md" : ""} `}
+          >
+            <Image
+              className={`${"flex h-full w-full rounded-xl object-cover lg:rounded-none"}`}
+              src={item.Pictures}
+              alt={item.Alt}
+            />
+          </div>
+>>>>>>> d8e1805 (Made car images in grid bigger while hovering)
         </motion.div>
       ))}
     </div>
