8211aa5921bf73d45eea00511562727fc9f5062f
etan29 <emilytan2992@gmail.com>
2026-02-26 20:31:03 -0800

    fix background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,14 @@ const Home = () => {
   return (
     <div className="relative min-h-screen w-full overflow-hidden">
       <HomeCover />
-      <section className="-mt-10 flex h-screen w-full flex-col items-center justify-center">
+      <div className="-mt-10 flex h-screen w-full flex-col items-center justify-center">
         <Moto />
-      </section>
-      <section id="about" className="relative h-screen min-h-screen">
+      </div>
+      <div id="about" className="relative min-h-screen">
         <AboutCover />
         <Title text="About Us" />
         <InfoSection />
-      </section>
+      </div>
     </div>
   );
 };
