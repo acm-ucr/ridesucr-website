6a0978033bf6c05e84378b8a18de46733121a216
Kel Umozurike <Kelechiumozurike@gmail.com>
2026-02-26 20:54:13 -0800

    Made the title responsive to different screen sizes

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,8 +4,10 @@ type TitleProps = {
 
 const Title = ({ text }: TitleProps) => {
   return (
-    <div className="flex items-center justify-center py-10 text-center text-7xl text-white uppercase">
-      {text}
+    <div className="flex items-center justify-center py-10 text-center text-7xl sm:py-8 md:py-10">
+      <div className="text-3x1 sm:text-5x1 md:text-6x1 text-white uppercase lg:text-7xl">
+        {text}
+      </div>
     </div>
   );
 };
