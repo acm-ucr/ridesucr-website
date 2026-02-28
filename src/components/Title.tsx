eb737960938588a7b35357dff23736725a0778af
ALE00111 <austintle56@gmail.com>
2026-02-28 13:19:59 -0800

    Fix

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,10 +4,8 @@ type TitleProps = {
 
 const Title = ({ text }: TitleProps) => {
   return (
-    <div className="flex items-center justify-center py-10 text-center text-7xl sm:py-8 md:py-10">
-      <div className="text-3x1 sm:text-5x1 md:text-6x1 text-white uppercase lg:text-7xl">
-        {text}
-      </div>
+    <div className="text-ridesucr-white flex justify-center py-10 text-center text-5xl md:text-7xl">
+      {text}
     </div>
   );
 };
