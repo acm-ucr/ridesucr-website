cffda7a042c4f921807e682cb40cec0e625fbeca
Celina Vasquez <cvasq043@ucr.edu>
2026-03-01 12:03:18 -0800

    Fixed comments

--- src/components/gallery/CarsGrid.tsx
+++ src/components/gallery/CarsGrid.tsx
@@ -3,21 +3,17 @@ import { motion } from "motion/react";
 import Image from "next/image";
 import { galleryData } from "@/data/galleryData";
 
-const imageAnimation = () => ({
-  whileHover: { scale: 1.05 },
-});
-
 const CarsGrid = () => {
   return (
-    <div className="grid h-fit grid-cols-1 gap-5 lg:h-[75rem] lg:grid-cols-3 lg:grid-rows-3 lg:gap-1.5">
+    <div className="grid h-auto grid-cols-1 gap-5 pb-3 lg:h-[76rem] lg:grid-cols-3 lg:grid-rows-3 lg:gap-x-1.5 lg:gap-y-4">
       {galleryData.map((item, index) => (
-        <motion.div key={index} {...imageAnimation()} className="">
+        <motion.div key={index} whileHover={{ scale:1.1, zIndex:70}} className="relative">
           <div
             key={index}
-            className={`lg:border-ridesucr-white ring-ridesucr-white/70 bg-ridesucr-gray/45 flex items-center justify-center overflow-hidden rounded-xl border p-[5px] shadow-2xl ring-1 backdrop-blur-md lg:rounded-none lg:bg-transparent lg:p-0 lg:shadow-none lg:ring-0 ${index == 0 ? "lg:rounded-tl-md" : ""} ${index == 2 ? "lg:rounded-tr-md" : ""} ${index == 6 ? "lg:rounded-bl-md" : ""} ${index == 8 ? "lg:rounded-br-md" : ""} `}
+            className={`lg:border-ridesucr-white ring-ridesucr-white/70 bg-ridesucr-gray/45 flex items-center justify-center rounded-xl border p-[5px] shadow-2xl ring-1 backdrop-blur-md lg:rounded-none lg:bg-transparent lg:p-0 lg:shadow-none lg:ring-0 ${index == 0 ? "lg:rounded-tl-md" : ""} ${index == 2 ? "lg:rounded-tr-md" : ""} ${index == 6 ? "lg:rounded-bl-md" : ""} ${index == 8 ? "lg:rounded-br-md" : ""} `}
           >
             <Image
-              className={`${"flex h-full w-full rounded-xl object-cover lg:rounded-none"}`}
+              className={`${"object-cover h-full w-full rounded-xl lg:rounded-none"}`}
               src={item.Pictures}
               alt={item.Alt}
             />
