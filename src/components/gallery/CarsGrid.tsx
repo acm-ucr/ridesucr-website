d599d431a6a1298201e6c59a838997d569f1b6e6
Celina Vasquez <cvasq043@ucr.edu>
2026-03-01 12:11:12 -0800

    Fixed comments

--- src/components/gallery/CarsGrid.tsx
+++ src/components/gallery/CarsGrid.tsx
@@ -30,7 +30,7 @@ const CarsGrid = () => {
             className={`lg:border-ridesucr-white ring-ridesucr-white/70 bg-ridesucr-gray/45 flex items-center justify-center rounded-xl border p-[5px] shadow-2xl ring-1 backdrop-blur-md lg:rounded-none lg:bg-transparent lg:p-0 lg:shadow-none lg:ring-0 ${index == 0 ? "lg:rounded-tl-md" : ""} ${index == 2 ? "lg:rounded-tr-md" : ""} ${index == 6 ? "lg:rounded-bl-md" : ""} ${index == 8 ? "lg:rounded-br-md" : ""} `}
           >
             <Image
-              className={`${"object-cover h-full w-full rounded-xl lg:rounded-none"}`}
+              className={`${"h-full w-full rounded-xl object-cover lg:rounded-none"}`}
               src={item.Pictures}
               alt={item.Alt}
             />
