1d277fc671c1f8d798b5571921a459a4acdda0eb
Celina Vasquez <cvasq043@ucr.edu>
2026-03-01 20:41:32 -0800

    Fixed spacing on bottom and errors

--- src/components/gallery/Garage.tsx
+++ src/components/gallery/Garage.tsx
@@ -2,8 +2,10 @@ import CarsGrid from "@/components/gallery/CarsGrid";
 
 const Garage = () => {
   return (
-    <div className="lg:ring-ridesucr-white/70 lg:bg-ridesucr-gray/45 lg:shadow-2x1 lg:rounded-xl lg:p-[5px] lg:ring-1 lg:backdrop-blur-md">
-      <CarsGrid />
+    <div className="lg:justify-center lg:pb-4">
+      <div className="lg:ring-ridesucr-white/70 lg:bg-ridesucr-gray/45 lg:shadow-2x1 lg:rounded-xl lg:p-[5px] lg:ring-1 lg:backdrop-blur-md">
+        <CarsGrid />
+      </div>
     </div>
   );
 };
