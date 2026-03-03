8ba84520e742ff658f8c1e1fcbac9010efebdf3f
etan29 <emilytan2992@gmail.com>
2026-03-03 01:28:06 -0800

    about us button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,10 @@ const Home = () => {
   return (
     <div className="relative min-h-screen w-full overflow-hidden">
       <HomeCover />
-      <div id="moto" className="-mt-10 flex h-screen w-full flex-col items-center justify-center">
+      <div
+        id="moto"
+        className="-mt-10 flex h-screen w-full flex-col items-center justify-center"
+      >
         <Moto />
       </div>
       <div id="about" className="relative min-h-screen">
