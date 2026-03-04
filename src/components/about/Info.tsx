994cf3d1de58701194d42a0e09ef83e2e40971f2
David-Li372 <138244633+David-Li372@users.noreply.github.com>
2026-03-04 06:36:44 -0800

    changes done

--- src/components/about/Info.tsx
+++ src/components/about/Info.tsx
@@ -1,7 +1,7 @@
 const Info = (props: { info: string }) => {
   return (
-    <div className="text-ridesucr-white flex h-full w-full items-center justify-center rounded-md bg-white/30 p-6 text-center text-base leading-6 backdrop-blur-lg sm:text-lg sm:leading-7 md:text-2xl md:leading-8 lg:text-4xl lg:leading-[1.3] xl:text-5xl xl:leading-[1.2]">
-      <div className="max-w-prose">{props.info}</div>
+    <div className="text-ridesucr-white lgbackdrop-blur-lg flex h-full w-full items-center justify-center rounded-md bg-white/30 p-6 text-center text-base leading-6 sm:text-2xl sm:leading-7 md:text-3xl md:leading-8 lg:text-4xl lg:leading-[1.6]">
+      <div className="">{props.info}</div>
     </div>
   );
 };
