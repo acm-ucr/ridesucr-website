1c7f034a10f8b3c6ef0f0ed46647c61cdeef071c
JeffreyXia05 <jeffx2016@gmail.com>
2026-03-04 00:51:18 -0800

    small fix

--- src/components/gallery/CarsGrid.tsx
+++ src/components/gallery/CarsGrid.tsx
@@ -6,23 +6,19 @@ import { galleryData } from "@/data/galleryData";
 
 const CarsGrid = () => {
   return (
-    <div className="grid h-auto grid-cols-1 gap-5 pb-3 lg:h-[76rem] lg:grid-cols-3 lg:grid-rows-3 lg:gap-x-1.5 lg:gap-y-4">
+    <div className="grid h-auto grid-cols-1 gap-5 lg:h-[76rem] lg:grid-cols-3 lg:grid-rows-3 lg:gap-x-1.5 lg:gap-y-1.5">
       {galleryData.map((item, index) => (
         <motion.div
           key={index}
           whileHover={{ scale: 1.1, zIndex: 70 }}
-          className="relative"
+          whileTap={{ scale: 1.1, zIndex: 70 }}
+          className={`lg:border-ridesucr-white ring-ridesucr-white/70 bg-ridesucr-gray/45 flex items-center justify-center rounded-xl border p-[5px] shadow-2xl ring-1 backdrop-blur-md lg:rounded-none lg:bg-transparent lg:p-0 lg:shadow-none lg:ring-0 ${index == 0 ? "lg:rounded-tl-md" : ""} ${index == 2 ? "lg:rounded-tr-md" : ""} ${index == 6 ? "lg:rounded-bl-md" : ""} ${index == 8 ? "lg:rounded-br-md" : ""} `}
         >
-          <div
-            key={index}
-            className={`lg:border-ridesucr-white ring-ridesucr-white/70 bg-ridesucr-gray/45 flex items-center justify-center rounded-xl border p-[5px] shadow-2xl ring-1 backdrop-blur-md lg:rounded-none lg:bg-transparent lg:p-0 lg:shadow-none lg:ring-0 ${index == 0 ? "lg:rounded-tl-md" : ""} ${index == 2 ? "lg:rounded-tr-md" : ""} ${index == 6 ? "lg:rounded-bl-md" : ""} ${index == 8 ? "lg:rounded-br-md" : ""} `}
-          >
-            <Image
-              className={`${"h-full w-full rounded-xl object-cover lg:rounded-none"}`}
-              src={item.Pictures}
-              alt={item.Alt}
-            />
-          </div>
+          <Image
+            className={`${"h-full w-full rounded-xl object-cover lg:rounded-none"}`}
+            src={item.Pictures}
+            alt={item.Alt}
+          />
         </motion.div>
       ))}
     </div>
