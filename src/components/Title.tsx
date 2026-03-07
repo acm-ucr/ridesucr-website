aedc95b46b4dc24c88c9905dc200dfa5cd58a98f
Kel Umozurike <Kelechiumozurike@gmail.com>
2026-03-06 16:59:29 -0800

    created an animation for title components

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,10 +1,14 @@
+"use client";
+
+import { motion } from "framer-motion";
+
 type TitleProps = {
   text: string;
 };
 
 const Title = ({ text }: TitleProps) => {
   return (
-    <div className="text-ridesucr-white flex justify-center py-10 text-center text-5xl md:text-7xl">
+    <div className="title-fade-up text-ridesucr-white flex justify-center py-10 text-center text-5xl md:text-7xl">
       {text}
     </div>
   );
