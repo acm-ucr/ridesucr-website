79f5718391b34edd4d16c32b622b2cf15a8f8374
ALE00111 <austintle56@gmail.com>
2026-03-09 11:11:00 -0700

    Fix rebase

--- src/components/gallery/CarsGrid.tsx
+++ src/components/gallery/CarsGrid.tsx
@@ -1,8 +1,5 @@
 "use client";
-<<<<<<< HEAD
 
-=======
->>>>>>> d8e1805 (Made car images in grid bigger while hovering)
 import { motion } from "motion/react";
 import Image from "next/image";
 import { galleryData } from "@/data/galleryData";
@@ -11,11 +8,10 @@ const CarsGrid = () => {
   return (
     <div className="grid h-auto grid-cols-1 gap-5 lg:h-[76rem] lg:grid-cols-3 lg:grid-rows-3 lg:gap-x-1.5 lg:gap-y-1.5">
       {galleryData.map((item, index) => (
-<<<<<<< HEAD
         <motion.div
           key={index}
-          whileHover={{ scale: 1.1, zIndex: 70 }}
-          whileTap={{ scale: 1.1, zIndex: 70 }}
+          whileHover={{ scale: 1.05, zIndex: 70 }}
+          whileTap={{ scale: 1.05, zIndex: 70 }}
           className={`lg:border-ridesucr-white ring-ridesucr-white/70 bg-ridesucr-gray/45 flex items-center justify-center rounded-xl border p-[5px] shadow-2xl ring-1 backdrop-blur-md lg:rounded-none lg:bg-transparent lg:p-0 lg:shadow-none lg:ring-0 ${index == 0 ? "lg:rounded-tl-md" : ""} ${index == 2 ? "lg:rounded-tr-md" : ""} ${index == 6 ? "lg:rounded-bl-md" : ""} ${index == 8 ? "lg:rounded-br-md" : ""} `}
         >
           <Image
@@ -23,19 +19,6 @@ const CarsGrid = () => {
             src={item.Pictures}
             alt={item.Alt}
           />
-=======
-        <motion.div key={index} {...imageAnimation()} className="">
-          <div
-            key={index}
-            className={`lg:border-ridesucr-white ring-ridesucr-white/70 bg-ridesucr-gray/45 flex items-center justify-center rounded-xl border p-[5px] shadow-2xl ring-1 backdrop-blur-md lg:rounded-none lg:bg-transparent lg:p-0 lg:shadow-none lg:ring-0 ${index == 0 ? "lg:rounded-tl-md" : ""} ${index == 2 ? "lg:rounded-tr-md" : ""} ${index == 6 ? "lg:rounded-bl-md" : ""} ${index == 8 ? "lg:rounded-br-md" : ""} `}
-          >
-            <Image
-              className={`${"h-full w-full rounded-xl object-cover lg:rounded-none"}`}
-              src={item.Pictures}
-              alt={item.Alt}
-            />
-          </div>
->>>>>>> d8e1805 (Made car images in grid bigger while hovering)
         </motion.div>
       ))}
     </div>
