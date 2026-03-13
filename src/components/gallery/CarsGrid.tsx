cda005e28a09f52a24a315e58ab673fba1426778
Celina Vasquez <cvasq043@ucr.edu>
2026-03-13 09:24:45 -0700

    Added animation to smoothly load in each image in gallery grid

--- src/components/gallery/CarsGrid.tsx
+++ src/components/gallery/CarsGrid.tsx
@@ -10,15 +10,23 @@ const CarsGrid = () => {
       {galleryData.map((item, index) => (
         <motion.div
           key={index}
-          whileHover={{ scale: 1.05, zIndex: 70 }}
-          whileTap={{ scale: 1.05, zIndex: 70 }}
-          className={`lg:border-ridesucr-white ring-ridesucr-white/70 bg-ridesucr-gray/45 flex items-center justify-center rounded-xl border p-[5px] shadow-2xl ring-1 backdrop-blur-md lg:rounded-none lg:bg-transparent lg:p-0 lg:shadow-none lg:ring-0 ${index == 0 ? "lg:rounded-tl-md" : ""} ${index == 2 ? "lg:rounded-tr-md" : ""} ${index == 6 ? "lg:rounded-bl-md" : ""} ${index == 8 ? "lg:rounded-br-md" : ""} `}
+          initial={{ opacity: 0, y: 30 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: index * 0.3 }}
+          viewport={{ once: true }}
         >
-          <Image
-            className={`${"h-full w-full rounded-xl object-cover lg:rounded-none"}`}
-            src={item.Pictures}
-            alt={item.Alt}
-          />
+          <motion.div
+            key={index}
+            whileHover={{ scale: 1.05, zIndex: 70 }}
+            whileTap={{ scale: 1.05, zIndex: 70 }}
+            className={`lg:border-ridesucr-white ring-ridesucr-white/70 bg-ridesucr-gray/45 flex items-center justify-center rounded-xl border p-[5px] shadow-2xl ring-1 backdrop-blur-md lg:rounded-none lg:bg-transparent lg:p-0 lg:shadow-none lg:ring-0 ${index == 0 ? "lg:rounded-tl-md" : ""} ${index == 2 ? "lg:rounded-tr-md" : ""} ${index == 6 ? "lg:rounded-bl-md" : ""} ${index == 8 ? "lg:rounded-br-md" : ""} `}
+          >
+            <Image
+              className={`${"h-full w-full rounded-xl object-cover lg:rounded-none"}`}
+              src={item.Pictures}
+              alt={item.Alt}
+            />
+          </motion.div>
         </motion.div>
       ))}
     </div>
