63f510e0ae7445ecee43fc49722484d9e15aea91
Kel Umozurike <Kelechiumozurike@gmail.com>
2026-03-22 14:50:32 -0700

    fix formatting

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -11,7 +11,7 @@ const Title = ({ text }: TitleProps) => {
     <motion.h1
       initial={{ opacity: 0, y: -20 }}
       animate={{ opacity: 1, y: 0 }}
-      transition = {{ duration: 0.5}}
+      transition={{ duration: 0.5 }}
       className="title-fade-up text-ridesucr-white flex justify-center py-10 text-center text-5xl md:text-7xl"
     >
       {text}
